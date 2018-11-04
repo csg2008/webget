3df92dc8224d3b4e84adbce667e1027be8d2dbb5
chen.s.g <chensg@imohe.com>
2018-11-04 17:13:33 +0800

    同类函数合并

--- util/filesystem.go
+++ util/filesystem.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
@@ -179,4 +180,40 @@ func FilePutContents(filename string, content []byte, append bool) error {
 
 	_, err = fp.Write(content)
 	return err
+}
+
+// SafeFileName replace all illegal chars to a underline char
+func SafeFileName(fileName string) string {
+	return strings.Map(func(r rune) rune {
+		if strings.IndexRune(`/\:*?"><|`, r) != -1 {
+			return '_'
+		}
+		return r
+	}, fileName)
+}
+
+// GetDirFiles 获取指定文件夹文件列表
+func GetDirFiles(dirPath string, stripExt bool) []string {
+	var ret []string
+
+	if files, err := ioutil.ReadDir(dirPath); nil == err && nil != files {
+		ret = make([]string, 0, len(files))
+		for _, v := range files {
+			if !v.IsDir() {
+				if stripExt {
+					var tmp = v.Name()
+					var idx = strings.LastIndexByte(v.Name(), '.')
+					if idx > 0 {
+						ret = append(ret, strings.Trim(tmp[0:idx], " "))
+					} else {
+						ret = append(ret, v.Name())
+					}
+				} else {
+					ret = append(ret, v.Name())
+				}
+			}
+		}
+	}
+
+	return ret
 }
